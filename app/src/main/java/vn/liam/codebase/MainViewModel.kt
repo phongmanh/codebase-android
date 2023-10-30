39bc5fdc0317306c0e251caab8208759d1e798f4
Manh Nguyen <phongmanhvn@gmail.com>
2023-10-30 10:28:42 +0700

    Added navigation type safe

---
+++ app/src/main/java/vn/liam/codebase/MainViewModel.kt
@@ -0,0 +1,19 @@
+package vn.liam.codebase
+
+import android.os.Bundle
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+
+    fun getArgument(arg: Bundle?): Any? {
+       return savedStateHandle.get<Int>("fragmentId")
+    }
+
+}
--- app/src/main/java/vn/liam/codebase/ui/home/HomeFragment.kt
+++ app/src/main/java/vn/liam/codebase/ui/home/HomeFragment.kt
@@ -6,9 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import dagger.hilt.android.AndroidEntryPoint
+import vn.liam.codebase.MainViewModel
 import vn.liam.codebase.databinding.FragmentHomeBinding
 
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
@@ -16,6 +20,7 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,7 +28,11 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this)[HomeViewModel::class.java]
+        arguments?.let {
+            println("Arg: ${HomeFragmentArgs.fromBundle(it).fragmentId}")
+            println("ArgView: ${viewModel.getArgument(arguments)}")
+        }
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
